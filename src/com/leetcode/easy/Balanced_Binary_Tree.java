57fd858bde1e90704e5401c3173cf79f7e1c2afa
goldenlightning <goldenlightning@163.com>
2017-03-27 16:01:10 +0800

    平衡树

---
+++ src/com/leetcode/easy/Balanced_Binary_Tree.java
@@ -0,0 +1,76 @@
+package com.leetcode.easy;
+
+import com.leetcode.medium.TreeNode;
+
+/**
+ * 【题目】Given a binary tree, determine if it is height-balanced.
+ * 
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the 
+ * depth of the two subtrees of every node never differ by more than 1.
+ * @author huangjl
+ *
+ */
+public class Balanced_Binary_Tree {
+	
+	public static void main(String[] args) {
+		
+		
+		TreeNode f = new TreeNode("f");
+		TreeNode d = new TreeNode(f, null, "d");
+		TreeNode e = new TreeNode("e");
+		
+		TreeNode b = new TreeNode(d, e, "b");
+		TreeNode c = new TreeNode(null, null, "c");
+		
+		TreeNode a = new TreeNode(b, c, "a");
+		
+		System.out.println(isBalanced2(a));
+		
+//		TreeNode d = new TreeNode("d");
+//		TreeNode c = new TreeNode(d, null, "c");
+//		TreeNode b = new TreeNode(c, null, "b");
+//		
+//		TreeNode a = new TreeNode(null, b, "a");
+//		
+//		System.out.println(isBalanced2(a));
+		
+	}
+	
+	public static boolean isBalanced2 (TreeNode node) {
+		if (node == null)
+			return true;
+ 
+		try {
+			getHeight(node);
+		} catch (RuntimeException ex) {
+			return false;
+		}
+ 
+		return true;
+	}
+
+	public static boolean isBalanced(TreeNode root) {
+		try {
+			System.out.println(getHeight(root));
+		} catch (RuntimeException e) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	private static int getHeight(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		
+		int left = getHeight(root.left);
+		int right = getHeight(root.right);
+		
+		if (Math.abs(left - right) > 1)
+			throw new RuntimeException(left - right + "");
+
+		return Math.max(left, right) + 1;
+	}
+	
+}
